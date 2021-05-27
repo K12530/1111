2aebee67e31644a1ca87a2eb2125847aaf19d751
yuankai <K1253042283@gmail.com>
2021-05-27 14:06:06 +0800

    修改Add函数

--- FourOperator/Add/Add.c
+++ FourOperator/Add/Add.c
@@ -1,15 +1,7 @@
 #include "Add.h"
 
-#define INT_MAX 2147483647
-#define INT_MIN -2147483647
-
 int Add(int a, int b)
 {
-	if ((a > INT_MAX) || (a < INT_MIN) || (b > INT_MAX) || (b < INT_MIN))
-	{
-		printf("输入溢出\r\n");
-		return 0;
-	}
 	if ((a > 0 && b > 0 && a + b <= 0) || (a < 0 && b < 0 && a + b >= 0))
 	{
 		printf("输出溢出\r\n");
